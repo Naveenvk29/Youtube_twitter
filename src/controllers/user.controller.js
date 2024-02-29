20c84ebb8c0966bbe292485e6c696bad4a928366
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-02-29 22:48:47 +0530

    add middle in userroutes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -40,16 +40,6 @@ const registerUser = asyncHandler(async (req, res) => {
   //console.log(req.files);
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
-  let coverImageLocalPath;
-  if (
-    req.files &&
-    Array.isArray(req.files.coverImage) &&
-    req.files.coverImage.length > 0
-  ) {
-    coverImageLocalPath = req.files.coverImage[0].path;
-  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -83,4 +73,5 @@ const registerUser = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
+
 export { registerUser };
